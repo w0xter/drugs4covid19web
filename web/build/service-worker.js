e6bed87599e417d32fc994bd1254f3a3842a2e73
w0xter <luispozogilo@gmail.com>
2020-04-05 17:17:39 +0200

    Adding build version

--- web/build/service-worker.js
+++ web/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6b1133d6f6a6f1ee4f7aa6f9de69c5fe.js"
+  "/precache-manifest.935f036dc4347f5c173fc9dc117c7626.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
