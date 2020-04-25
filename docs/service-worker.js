254fc2470a7dc12327c5e43a8a791306eb102aa1
w0xter <luispozogilo@gmail.com>
2020-04-25 10:46:34 +0200

    Preparing Deploy

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.899271f02582875a4624de4cf66e6977.js"
+  "/precache-manifest.a985522ad84232ecb192375bb26b445b.js"
 );
 
 self.addEventListener('message', (event) => {
